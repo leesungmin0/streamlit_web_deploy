0f9ffdc037a16aa9d5fb1843d8ac6986e1cd6bdc
leesungmin0 <131140356+leesungmin0@users.noreply.github.com>
2023-04-23 16:50:39 +0900

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,116 +1,50 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[16]:
-
-
+import streamlit as st
+from streamlit_chat import message
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import json
 
+@st.cache(alllow_output_mutation=True)
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+  
+@st.cache(alllow_output_mutation=True)
+def cached_model():
+    model = pd.read_csv('wellness_dataset.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+  
+model = cached_model()
+df = get_dataset()
+  
+st.header('심리상담 챗봇')
+st.markdown("[잇다_진로공유플랫폼]")
 
-# In[11]:
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
 
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 
-get_ipython().system('pip install sentencepiece')
-get_ipython().system('pip install pandas')
-get_ipython().system('pip install sentence-transformers')
-get_ipython().system('pip install scikit-learn')
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('당신: ', '')
+    submitted = st.form_submit_button('전송')
 
-get_ipython().system('pip install scikit-image')
+if submitted and user_input:
+    embedding = model.encode(user_input)
 
+    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['distance'].idxmax()]
 
-# In[17]:
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer['챗봇'])
 
-
-model = SentenceTransformer('jhgan/ko-sroberta-multitask')
-
-sentences = ["안녕하세요?", "한국어 문장 임베딩을 위한 버트 모델입니다."]
-embeddings = model.encode(sentences)
-
-print(embeddings)
-
-
-# In[18]:
-
-
-df = pd.read_csv('wellness_dataset_original.csv')
-
-df.head()
-
-
-# In[19]:
-
-
-df = df.drop(columns=['Unnamed: 3'])
-
-df.head()
-
-
-# In[20]:
-
-
-df = df[~df['챗봇'].isna()]
-
-df.head()
-
-
-# In[21]:
-
-
-df.loc[0, '유저']
-
-
-# In[22]:
-
-
-model.encode(df.loc[0, '유저'])
-
-
-# In[23]:
-
-
-df['embedding'] = pd.Series([[]] * len(df)) # dummy
-
-df['embedding'] = df['유저'].map(lambda x: list(model.encode(x)))
-
-df.head()
-
-
-# In[24]:
-
-
-df.to_csv('wellness_dataset.csv', index=False)
-
-
-# In[25]:
-
-
-text = '요즘 머리가 아프고 너무 힘들어'
-
-embedding = model.encode(text)
-
-
-# In[26]:
-
-
-df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
-
-df.head()
-
-
-# In[27]:
-
-
-answer = df.loc[df['distance'].idxmax()]
-
-print('구분', answer['구분'])
-print('유사한 질문', answer['유저'])
-print('챗봇 답변', answer['챗봇'])
-print('유사도', answer['distance'])
-
-
-# In[ ]:
+for i in range(len(st.session_state['past'])):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
 
 
 
